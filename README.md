54cd8254cf48c87053d029d4fa99f53c4190cc15
Jen Vitrano <vitranojen@gmail.com>
2016-02-05 20:24:24 -0500

    Add JavaScript for homework
    Added presidents, array, and functions branch

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 
+#Javascript Homework
+
+[My GitHub repo link](https://github.com/jv13/hw_javascript_vitrano_jen.git)
+
+##Sources
+
+* I used [W3 Schools](http://www.w3schools.com/json/json_syntax.asp) to help me with the presidents branch array.  Through using this link I was able to see that I needed to switch all of the keys in the objects to strings in order to get the result of Bill Clinton.
+
+##Comments
+* This assignment was challenging, but I was excited when the console created an output I was looking for.  Originally, I was stuck on the presidents branch array.  I had to walk away and revisit the next day and that is when I was able to to output 'Bill Clinton'.  
+* I do not believe my inception function is correct, although I know it logs out the correct output.  I continually received the error "callback is not a function".  I tried to re-watch lectures and Lynda.com videos but I just could not figure it out.
+* I also added comments to the JavaScript so I understood where the different branches began.
---
+++ js/main.js
@@ -0,0 +1,50 @@
+//arrays branch
+var iceCream = [ 'Strawberry', 'Chocolate', 'Cookies and Cream' ];
+var newLength = iceCream.push( 'Rocky Road' );
+
+//presidents branch 
+var presidents = [
+		{ 'firstName': 'Barack', 'lastName': 'Obama',
+		'termLength': '2009 - Present', 'party': 'Democratic', 'yearsOfPresidency': 8 },
+
+		{ 'firstName': 'George W', 'lastName': 'Bush',
+		'termLength': '2001 - 2009', 'party': 'Republican', 'yearsOfPresidency': 8 },
+
+		{ 'firstName': 'Bill', 'lastName': 'Clinton',
+		'termLength': '1993 - 2001', 'party': 'Democratic', 'yearsOfPresidency': 8 },
+
+		{ 'firstName': 'George H. W', 'lastName': 'Bush',
+		'termLength': '1989 - 1993', 'party': 'Republican', 'yearsOfPresidency': 4 },
+
+		{ 'firstName': 'Ronald', 'lastName': 'Reagan',
+		'termLength': '1981 - 1989', 'party': 'Republican', 'yearsOfPresidency': 8 }
+	];
+console.log( presidents[ 2 ].firstName + ' ' + presidents[ 2 ].lastName ) ;
+
+//functions branch
+function howdy( person, message ) {
+	return person + 'says' + message;
+}
+var message = howdy( 'Jen Vitrano ', ' howdy!' );
+console.log( message ) ;
+
+//new numba function
+var numba = function( string ) {
+	if ( string.length < 7 ) {
+		console.log( 'What a short little word!' );
+	} else if ( string.length > 7 ) {
+		console.log( 'Im sorry, but thats too many to count.' );
+	} else if ( string.length == 7 ) {
+			console.log( '7, what a perfect choice!' );}
+};
+console.log( numba( '7' ) );
+console.log( numba( 'seventy' ) );
+console.log( numba( 'university' ) ) ;
+
+//inception function
+function inception( callback, value ) {
+	callback( value );
+}
+var callback = 'Harry Potter';
+var value = 'is a fantastic movie.';
+console.log( callback, value );
